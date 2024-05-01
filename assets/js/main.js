65e6e118914142b846a439df7d6ba6c1b9fcb4fb
Wesley Gomes <wesleytsst@outlook.com>
2024-05-01 14:25:58 -0300

    feat: animate site

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+/* Animacoes do nosso site https://gsap.com/ */
+/* imagem da mulher na home */
+gsap.from(".home__cover", 1.5, { opacity: 0, y: 10, delay: 0.1 });
+
+/* dados da home */
+gsap.from(".home__data", 1.8, { opacity: 0, x: -100, delay: 0.2 });
+
+/* grid de vantagens */
+gsap.from(".advantages__item", 1.5, { opacity: 0, y: 150, delay: 0.2 });
+/* cartoes */
+gsap.from(".advantages__blue", 1.5, { opacity: 0, y: 150, delay: 0.2 });
+gsap.from(".advantages__pink", 1.5, { opacity: 0, y: 150, delay: 0.1 });
+gsap.from(".advantages__gray", 1.5, { opacity: 0, y: 150, delay: 0.4 });
+gsap.from(".advantages__gold", 1.5, { opacity: 0, y: 150, delay: 0.6 });
+
+/* animar o footer */
+gsap.from(".footer__header", 1.5, { opacity: 0, y: 150, delay: 0.2 });
+gsap.from(".footer__data", 1.5, { opacity: 0, y: 150, delay: 0.8 });
